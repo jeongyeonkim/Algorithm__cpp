08d7b54621f9aaacb2033dba2e7964f694cf080e
jeongyeon <yeon8480@gmail.com>
2019-01-02 18:54:24 +0900

    -

--- practice/src/Baekjoon_2741.java
+++ practice/src/Baekjoon_2741.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class Baekjoon_2741 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		
+		for(int i=1; i <= number; i++)
+			System.out.println(i);
 	}
 
 }
