7a662bfe2d8948b5021779bb453905e8b65d3910
jeongyeon <yeon8480@gmail.com>
2019-12-31 19:34:42 +0900

    -

--- jeongyeon/samsung_Academy/D3/1206.cpp
+++ jeongyeon/samsung_Academy/D3/1206.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+int t;
 
 int main(void){
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    
+    int Tcase, result;
+
+    for(int t=1; t<=10; t++){
+        Tcase = 0; result = 0;
+        cin >> Tcase;
+
+        int arr[Tcase+1];
+        for(int i=0; i<Tcase; i++){
+            cin >> arr[i];
+        }
+
+        for(int i=2; i<Tcase-2; i++){
+            if(arr[i] > arr[i-1] && arr[i] > arr[i-2] && arr[i] > arr[i+1] && arr[i] > arr[i+2]){
+                int temp;
+                temp = max(arr[i-2], max(arr[i-1], max(arr[i+1], arr[i+2])));
+                result += arr[i] - temp;
+            }
+        }
+        cout << "#" << t << " " << result << "\n";
+    }
 }
