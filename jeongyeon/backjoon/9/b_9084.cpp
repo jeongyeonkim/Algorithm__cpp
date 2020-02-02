c88144c6606cd21f3b5d74cd6319f3ecd899e5ca
jeongyeon <yeon8480@gmail.com>
2020-02-02 16:01:13 +0900

    -

--- jeongyeon/backjoon/9/b_9084.cpp
+++ jeongyeon/backjoon/9/b_9084.cpp
@@ -3,10 +3,18 @@
 #include <string.h>
 using namespace std;
 int T, N, M;
-int coin[6], result[21];
+int coin[20], result[10001];
 
 int cntCoin(){
-    
+    result[0]=1;
+
+    for(int i=0; i<=N; i++){
+        for(int j=coin[i]; j<=M; j++){
+            result[j] += result[j-coin[i]];
+        }
+    }
+
+    return result[M];
 }
 
 int main(){
@@ -16,12 +24,9 @@ int main(){
     for(int i=0; i<T; i++){
         cin >> N;
         memset(coin, 0, sizeof(coin));
-        for(int j=0; j<N; i++){ cin >> coin[i]; }
+        for(int j=0; j<N; j++){ cin >> coin[i]; }
         cin >> M;
-        result[i] = cntCoin();
-    }
 
-    for(int i=0; i<T; i++){
-        cout << result[i] << "\n";
+        cout << cntCoin() << "\n";
     }
 }
