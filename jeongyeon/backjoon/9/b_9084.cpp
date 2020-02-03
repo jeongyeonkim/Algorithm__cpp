64f457a72be6e1f151ceae9a64741574544331df
jeongyeon <yeon8480@gmail.com>
2020-02-03 23:54:30 +0900

    -

--- jeongyeon/backjoon/9/b_9084.cpp
+++ jeongyeon/backjoon/9/b_9084.cpp
@@ -1,36 +1,28 @@
-// 동전
+// // 동전
 #include <iostream>
-#include <cstring>
+#include <string.h>
 using namespace std;
-int T, N, M;
-int coin[20], result[10001], output[10];
-
-int cntCoin(){
-    memset(result, 0, sizeof(result));
-    result[0]=1;
-
-    for(int i=0; i<=N; i++){
-        for(int j=coin[i]; j<=M; j++){
-            result[j] += result[j-coin[i]];
+int N, M, T;
+int coin[20], result[10001];
+ 
+int maxCoin(void){
+        memset(result, 0, sizeof(result));
+        result[0] = 1;
+        for(int i=0; i<N; i++){
+            for (int j=coin[i]; j<=M; j++){
+               result[j] += result[j - coin[i]];
+           }
         }
-    }
-
-    return result[M];
+        return result[M];
 }
-
+ 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     cin >> T;
-
     for(int i=0; i<T; i++){
         cin >> N;
         memset(coin, 0, sizeof(coin));
-        for(int j=0; j<N; j++){ cin >> coin[j]; }
+        for(int i=0; i<N; i++){ cin >> coin[i]; }
         cin >> M;
-        output[i] = cntCoin();
-    }
-
-    for(int i=0; i<T; i++){
-        cout << output[i] << "\n";
+        cout << maxCoin() << "\n";
     }
 }
