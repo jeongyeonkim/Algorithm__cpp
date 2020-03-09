a073184662eca870bd1e014e5b13bb7dba92e4a9
jeongyeon <yeon8480@gmail.com>
2020-03-09 23:49:54 +0900

    -

---
+++ jeongyeon/backjoon/10/b_14890.cpp
@@ -0,0 +1,16 @@
+// 경사로
+#include <iostream>
+using namespace std;
+int N, L;
+int arr[101][101];
+
+int main(void){
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cin >> N >> L;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){ cin >> arr[i][j]; }
+    }
+
+    
+
+}
