8cb4dd431bf0b8b7da915072197274f3412cc085
jeongyeonkim <yeon8480@gmail.com>
2020-05-22 16:57:42 +0900

    -

---
+++ backjoon/10/b_10711.cpp
@@ -0,0 +1,53 @@
+// 모래성
+#include <iostream>
+#include <string.h>
+#include <queue>
+using namespace std;
+int N, M, result;
+int arr[1001][1001];
+int dx[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+int dy[8] = {1, 0, -1, 0, 1, -1, 1, -1};
+queue<pair <int, int> > pos;
+
+bool castle(){
+    int s = pos.size();
+    for(int i=0; i<s; i++){
+        int x = pos.front().first;
+        int y = pos.front().second;
+        pos.pop();
+
+        for(int j=0; j<8; j++){
+            int nx = x + dx[j];
+            int ny = y + dy[j];
+            if(nx < 0 || ny < 0 || nx >= N || ny >= M){ continue; }
+            if(arr[nx][ny] > 0){
+                arr[nx][ny]--;
+                if(!arr[nx][ny]){
+                    pos.push(make_pair(nx, ny));
+                }
+            }
+        }
+
+    }
+    if(pos.empty()){ return false; }
+    return true;
+}
+
+int main(void){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        string str; cin >> str;
+        for(int j=0; j<M; j++){
+            if(str[j] != '.'){
+                arr[i][j] = str[j] - '0';
+            }else{
+                pos.push(make_pair(i, j));
+            }
+        }
+    }
+
+    while (castle()){ result++; }
+    cout << result;
+    return 0;
+}
--- backjoon/10/b_17244.cpp
+++ backjoon/10/b_17244.cpp
@@ -1,9 +1,60 @@
 // 아 맞다 우산
+// 1194 - 달이 차오른다. 가자  >>> 이문제랑 거의 똑같아!
 #include <iostream>
+#include <string>
+#include <queue>
 using namespace std;
-int arr[51][51];
+int N, M, startX, startY, cntX, result;
+char arr[51][51];
+int visited[51][51][1<<5]; // 방문했던 물건 위치 비트마스크로 저장
+int dx[4] = {0, 1, -1, 0};
+int dy[4] = {1, 0, 0, -1};
+
+struct info{
+    int x, y, k, cnt;
+};
 
 int main(void){
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    
+    cin >> M >> N;
+    for(int i=0; i<N; i++){
+        string str; cin >> str;
+        for(int j=0; j<M; j++){
+            arr[i][j] = str[j];
+            if(str[j] == 'X'){ arr[i][j] = cntX++ + '0'; }
+            else if(str[j] == 'S'){ startX = i; startY = j; }
+        }
+    }
+    queue<info> que;
+    que.push({startX, startY, 0, 0});
+    visited[startX][startY][0] = 1;
+
+    while (!que.empty()){
+        int x = que.front().x;
+        int y = que.front().y;
+        int k = que.front().k;
+        int cnt = que.front().cnt;
+        que.pop();
+
+        if(arr[x][y] == 'E' && k == (1 << cntX) - 1){
+            result = cnt;
+            break;
+        }
+
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int nk = k;
+
+            if(nx < 0 || ny < 0 || nx >= N || ny >= M || arr[nx][ny] == '#'){ continue; }
+            if(visited[nx][ny][nk]){ continue; }
+
+            if(arr[nx][ny] >= '0' && arr[nx][ny] <= '4'){ nk |= (1 << (arr[nx][ny] - '0')); }
+            visited[nx][ny][nk] = 1;
+            que.push({nx, ny, nk, cnt + 1});
+        }
+    }
+
+    cout << result;
+    return 0;
 }
