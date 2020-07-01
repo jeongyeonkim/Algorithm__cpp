593e51eb4ec6f4079e82f37608ab3ac5ca3301e5
jeongyeonkim <yeon8480@gmail.com>
2020-07-01 17:09:14 +0900

    -

---
+++ backjoon/10/b_10800.cpp
@@ -0,0 +1,32 @@
+// 컬러볼
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N, C, S;
+int result[200001];
+vector<pair <int, int> > colorBoll;
+
+int main(void){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> N;
+    vector<int> arr[N + 1];
+    for(int i=0; i<N; i++){
+        cin >> C >> S;
+        colorBoll.push_back(make_pair(C, S));
+        arr[C].push_back(S);
+    }
+    //for(int i=1; i<=N; i++){ sort(arr[i].begin(), arr[i].end()); }
+    for(int i=0; i<N; i++){
+        for(int j=1; j<=N; j++){
+            if(j == colorBoll[i].first){ continue; }
+            for(int z=0; z<arr[j].size(); z++){
+                if(arr[j][z] >= colorBoll[i].second){ continue; }
+                result[i] += arr[j][z];
+            }
+        }
+    }
+
+    for(int i=0; i<N; i++){ cout << result[i] << "\n"; }
+    return 0;
+}
--- backjoon/2/b_2961.cpp
+++ backjoon/2/b_2961.cpp
@@ -2,22 +2,27 @@
 #include <iostream>
 using namespace std;
 int N, K;
-int arrN[11], resultK[11], num[361];
+int arrN[11], num[361], dp[11][361];
 
-void sol(){
-
+void sol(int idx, int sum){
+    num[sum] = 1;
+    if(idx == N){ return; }
+    if(dp[idx][sum] == 1){ return; }
+    dp[idx][sum] = 1;
+    sol(idx + 1, (sum + arrN[idx]) % 360);
+    sol(idx, (sum + arrN[idx]) % 360);
+    sol(idx + 1, (sum - arrN[idx] + 360) % 360);
 }
 
 int main(void){
     ios_base::sync_with_stdio(false);  cin.tie(0); cout.tie(0);
     cin >> N >> K;
     for(int i=0; i<N; i++){ cin >> arrN[i]; }
-    for(int i=0; i<K; i++){ cin >> resultK[i]; }
-
-
+    sol(0, 0);
 
     for(int i=0; i<K; i++){
-        if(num[resultK[i]] == 1){ cout << "YES\n"; }
+        int input; cin >> input;
+        if(num[input] == 1){ cout << "YES\n"; }
         else{ cout << "NO\n"; }
     }
     return 0;
